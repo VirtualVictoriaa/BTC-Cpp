ed06a0dcb0a6220d4b69bd3c903bb83b163781aa
Tori Ulm <victoriaa.allison08@gmail.com>
2025-05-20 10:48:39 -0700

    Updated some stuff

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -1,36 +1,71 @@
+// InsertionSort.cpp
+// https://www.youtube.com/watch?v=8oJS1BMKE64
+
 #include <iostream>
 
-void insertionSort(int arr[], int size) {
-    int i, j, key;
-    //forward iteration
-    for (int i = 1; i < size; i++) {
-        //key is the number we are trying to place
-        int key = arr[i];
-        //backward iterator (starts just ahead of i)
-        int j = i - 1;
+// Quick Sort
+// This function takes the last element as pivot, places the pivot element at its correct position in sorted array, and places all smaller (smaller than pivot) to left of pivot and all greater elements to right of pivot
+int partition (int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1); // index of smaller element
+    for (int j = low; j < high; j++) {
+        // If current element is smaller than or equal to pivot
+        if (arr[j] >= pivot) {
+            i++; // increment index of smaller element
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
 
-        // Move elements of arr[0..i-1], that are greater than key,
-        // to one position ahead of their current position
-        while (j >= 0 && arr[j] > key) {
-            //If the number is greater than key, we move it one position ahead
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        // pi is partitioning index, arr[pi] is now at right place
+        int pi = partition(arr, low, high);
+
+        // Recursively sort elements before partition and after partition
+        quickSort(arr, low, pi - 1); //Left of pivot(partition index)
+        quickSort(arr, pi + 1, high);//Right of pivot
+    }
+}
+
+// Insertion Sort
+void insertionSort(int arr[], int size)
+{
+    int i, j, key;
+    // iterate through the array, starting at position 1
+    for (i = 1; i < size; i++)
+    {
+        // key is the current item, we need to determine if it should move
+        key = arr[i];
+        // j is the previous index and will allow us to shift items forward
+        j = i - 1;
+        // Shift everything from arr[0..i-1] that is greater than the key
+        // No if's req'd as this guarantees everything ahead is already sorted
+        while (j >= 0 && arr[j] < key)
+        {
             arr[j + 1] = arr[j];
             j--;
         }
-        //Place the key in its correct position
         arr[j + 1] = key;
     }
 }
- 
-void print(int arr[], int size) {
-    for (int i = 0; i < size; i++)
+
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
         std::cout << arr[i] << " ";
-    std::cout << std::endl;
+    std::cout << '\n';
 }
 
-int main() {
-    int arr[] = {12, 11, 13, 5, 6};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    insertionSort(arr, size);
-    print(arr, size);
+int main()
+{
+    int numbers[] = { 9, 0, 5, 3, 8, 1, 4, 6, 7, 10, 2 };
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    quickSort(numbers, 0, size - 1);
+    print(numbers, size);
+
     return 0;
 }
