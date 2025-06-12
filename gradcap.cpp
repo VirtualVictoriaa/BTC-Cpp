0cbe718edc79ab26ab8ae8a99965df3922cf1041
Tori Ulm <victoriaa.allison08@gmail.com>
2025-06-11 18:49:30 -0700

    Grad Cap Decoration

---
+++ gradcap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int year = 2025;
+void computerNetworking(int year) {
+    if (year == 2025 ) {
+        std::cout << "WE DID IT!" << std::endl;
+    }
+}
+void cyberSecurity(int year) {
+    if (year == 2027) {
+        std::cout << "REAL TECH PRO!" << std::endl;
+    }
+}
+
+int main() {
+    computerNetworking(year);
+    cyberSecurity(year);
+    return 0;
+}
